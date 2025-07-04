5fda38d460692cd7d80210f5c0fbf4dd0cc0e75d
Nikita Demir <nikitde1@gmail.com>
2025-07-04 16:34:04 -0400

    exp

---
+++ experiments/truck_no_init_with_corr.sh
@@ -0,0 +1,5 @@
+python train.py \
+  train.gs_epochs=30000 \
+  init_wC.use=False \
+  gs.dataset.source_path=/root/data/tandt/truck \
+  gs.dataset.model_path=/root/output/truck_edgs_10k_matches_per_ref_no_init_with_corr
