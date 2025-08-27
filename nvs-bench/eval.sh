c4d9715bb0955cae092b1226023042b6ef04f235
Nikita Demir <nikitde1@gmail.com>
2025-08-27 17:02:49 -0400

    nvs-bench

---
+++ nvs-bench/eval.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if data_folder and output_folder arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <data_folder> <output_folder>"
+    echo "Example: $0 /nvs-bench-data/mipnerf360/bicycle /nvs-bench-output/mipnerf360/bicycle/ours"
+    exit 1
+fi
+data_folder=$1
+output_folder=$2
+
+
+######## START OF YOUR CODE ########
+# 1) Train 
+#   python train.py --data $data_folder --output $output_folder --eval
+# 2) Render the test split
+#   python render.py --data $data_folder/test --output $output_folder --eval
+# 3) Move the renders into `$output_folder/test_renders`
+#   mv $output_folder/test/ours_30000/renders $output_folder/test_renders
+
+python train.py wandb.mode="disabled" \
+    gs.dataset.source_path=$data_folder \
+    gs.dataset.model_path=$output_folder \
+    gs.dataset.eval=True \
+    init_wC.use=True \
+    init_wC.matches_per_ref=15_000 \
+    init_wC.nns_per_ref=3 \
+    init_wC.num_refs=180
+
+python ./submodules/gaussian-splatting/render.py \
+    --iteration 30000 \
+    -s $data_folder \
+    -m $output_folder \
+    --eval
+
+mv $output_folder/test/ours_30000/renders $output_folder/test_renders
--- nvs-bench/image.py
+++ nvs-bench/image.py
@@ -1,18 +1,24 @@
-###### How to edit this file ######
-# Docker and Dockerfiles are quite simple:
-# - a dockerfile is the set of instructions for getting a fresh machine ready to run your code
-# - start by defining a base image (FROM ...) based on the cuda and torch version you want. This gets the hard gpu driver stuff out of the way
-# - set env vars with ENV ..., change directories with WORKDIR ..., and run commands with RUN ...
-# - avoid using conda installs (just replace them with pip installs) because getting conda initialized in docker is a pain
-# 
-# Beam will handle building the docker image from this file, but you can also build it yourself and run it wherever you want
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 
-from modal import Image
+from modal import Image, Volume
+
+method_name = Path.cwd().name
+assert method_name != "nvs-bench", (
+    "nvs-bench must be called from the method's directory, not the nvs-bench subdirectory. Eg: `modal run nvs-bench/image.py`."
+)
+
+data_volume = Volume.from_name("nvs-bench-data", create_if_missing=True)
+output_volume = Volume.from_name("nvs-bench-output", create_if_missing=True)
+
+modal_volumes: dict[str | PurePosixPath, Volume] = {
+    "/nvs-bench-data": data_volume,
+    "/nvs-bench-output": output_volume,
+    # "/root/.cursor-server": Volume.from_name("cursor-volume", create_if_missing=True),
+}
+
 
 image = (
-    Image
-    .from_registry("pytorch/pytorch:2.4.1-cuda12.1-cudnn9-devel")
+    Image.from_registry("pytorch/pytorch:2.4.1-cuda12.1-cudnn9-devel")
     .env(
         {
             # Set Torch CUDA Compatbility to be for RTX 4090, T4, L40s, and A100
@@ -52,9 +58,8 @@ image = (
             && rm -rf /var/lib/apt/lists/*"
     )
     .workdir(f"/root/{Path.cwd().name}")
-
     ###### Your Code Here ######
-    # Probably easiest to pull the repo from github, but you can also copy files from your local machine with COPY 
+    # Probably easiest to pull the repo from github, but you can also copy files from your local machine with add_local_dir
     # eg: RUN git clone https://github.com/graphdeco-inria/gaussian-splatting.git . --recursive
     .run_commands("git clone -b nvs-leaderboard https://github.com/N-Demir/EDGS.git --recursive .")
     # Install (avoid conda installs because they don't work well in dockerfile situations)
@@ -70,7 +75,9 @@ image = (
     # Optionally install original colmap but probably pycolmap suffices
     # conda install conda-forge/label/colmap_dev::colmap
     # RUN pip install pycolmap
-    .run_commands("pip install wandb hydra-core tqdm torchmetrics lpips matplotlib rich plyfile imageio imageio-ffmpeg numpy==1.26.4")
+    .run_commands(
+        "pip install wandb hydra-core tqdm torchmetrics lpips matplotlib rich plyfile imageio imageio-ffmpeg numpy==1.26.4"
+    )
     # Stuff necessary for gradio and visualizations
     .run_commands("pip install gradio plotly scikit-learn moviepy==2.1.1 ffmpeg")
     .run_commands("pip install open3d")
